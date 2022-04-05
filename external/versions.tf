d7c89ca391f83718662c270b550fdf48f1a4d1d6
carp <carp@fullmetal.fish>
2022-04-06 00:24:01 +0200

    configure: fix terraform org

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "karpfediem"
+    organization = "karpfen"
 
     workspaces {
       name = "homelab-external"
